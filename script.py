6a6826e6b686bc90f159a62e40a5122b07569b78
Jaival Patel <pateljaival05@gmail.com>
2022-03-25 12:37:19 -0400

    layers init

--- script.py
+++ script.py
@@ -1,4 +1,20 @@
-
+from blockchain import Blockchain
 class L2:
+    L1 = Blockchain()
+    L2 = Blockchain()
     def __init__(self):
-        pass
+        while True:
+            try:
+                user_input = str(input(': '))
+                if user_input == '/quit':
+                    break 
+                elif user_input == '/add':
+                    self.add()
+                user_input = int(user_input)
+                
+            
+            except:
+                pass
+            
+    
+            
