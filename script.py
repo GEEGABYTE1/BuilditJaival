1fccad4d0254daf4191bb642a84c02d4de06042f
Jaival Patel <pateljaival05@gmail.com>
2022-03-26 23:51:45 -0400

    add value to specific parts of chain

--- script.py
+++ script.py
@@ -6,12 +6,16 @@ class L2:
         while True:
             try:
                 user_input = str(input(': '))
+                user_input = user_input.strip(' ')
                 if user_input == '/quit':
                     break 
                 elif user_input == '/add':
                     self.add()
-                
-                
+                elif '/add_val' in user_input:              #add_val [block-idx] 
+                    lst = user_input.split(' ')
+                    self.add_val(lst)
+                    
+                   
             
             except:
                 pass
@@ -22,6 +26,41 @@ class L2:
         for i in range(user_input):
             transaction = {'chain': self.L2_chain}
             self.L1.add_block(transaction)
+
+    def add_val(self, lst):
+        if len(lst) == 1:
+            print("There are not enough parameters")
+            return
+        val_to_add = None
+        for command in range(len(lst)):
+            if command == 0:
+                pass 
+            elif command == 1:
+                val = lst[command].strip(' ')
+                val_to_add = val
+            elif command == 2:
+                user_idx = lst[command].strip(' ')
+                user_idx = user_idx.strip('[')
+                user_idx = user_idx.strip(']')
+                user_idx = int(user_idx)
+                for block_idx in range(len(self.L1.chain)):
+                    if block_idx == user_idx:
+                        transaction = {'Val': val_to_add}
+                        network = self.L1.chain[block_idx]
+                        network.add_block(transaction)
+            else:
+                continue 
+        return True
+                        
+                    
+                    
+
+                
+                
+
+        
+    
+
         
 
 
